b257068f136384e82abbaa4a1fb8937aac45c7f9
Aesoppopotamus <the.recycled.hero@gmail.com>
2024-01-01 17:00:01 -0500

    commit

---
+++ my-website/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import combine from './combine.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="combine.png" className="App-logo" alt="logo" />
+      <p>Greetings, this is my first React Application!</p>
+      <p>May thy walk this new path gracefully, learn a lot, make mistakes, and expouse happiness.</p>
+    <a
+      className="App-links" 
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Learn React
+      </a>
+    </header> 
+    </div>
+  );
+};
+
+export default App;
